1db9dce1283566b805dbe35a6826bdce28bfca9d
geunhwa37 <jacika37@naver.com>
2024-11-11 22:15:02 +0900

    feat: InputView 클래스 구현

--- src/App.js
+++ src/App.js
@@ -55,7 +55,16 @@ class Cart {
   getItems() {
     return this.items;
   }
+}
 
+// InputView 클래스: 사용자의 입력을 처리
+class InputView {
+  static async readProduct() {
+    const input = await Console.readLineAsync("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]");
+    const [name, quantity] = input.split('-');
+    return { name, quantity: Number(quantity) };
+  }
+}
 
 
 
