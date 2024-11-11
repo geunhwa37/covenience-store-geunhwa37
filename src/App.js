079e1e09af05f2a729675aa565745940284fc846
geunhwa37 <jacika37@naver.com>
2024-11-11 21:42:06 +0900

    feat: 상품 가격 계산 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { Console, DateTimes } from '@woowacourse/mission-utils';
+
+// Product 클래스: 상품의 이름, 가격, 재고, 프로모션을 관리
+class Product {
+  constructor(name, price, stock, promotion = null) {
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+    this.promotion = promotion;
+  }
+  // 상품의 가격을 계산하는 메서드
+  calculatePrice(quantity) {
+    if (this.promotion) {
+      return this.applyPromotion(quantity);
+    }
+    return this.price * quantity;
+  }
+
+
+
 class App {
   async run() {}
 }
